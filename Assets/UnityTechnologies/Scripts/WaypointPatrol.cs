0c6fb296c721378fd77324f506274ee1ae0f4444
Richa Juvekar <richa.juvekar@gmail.com>
2024-05-09 15:15:08 -0400

    Began set up for WaypointPatrol script

--- Assets/UnityTechnologies/Scripts/WaypointPatrol.cs
+++ Assets/UnityTechnologies/Scripts/WaypointPatrol.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class WaypointPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
     void Start()
     {
-        
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
