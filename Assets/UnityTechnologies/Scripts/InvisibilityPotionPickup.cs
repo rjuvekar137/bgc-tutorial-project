1b528ab4c20c7174fa91e38d32581166d15d6776
Richa Juvekar <richa.juvekar@gmail.com>
2024-05-14 13:06:49 -0400

    Wrote basic script for invisibility collection

--- Assets/UnityTechnologies/Scripts/InvisibilityPotionPickup.cs
+++ Assets/UnityTechnologies/Scripts/InvisibilityPotionPickup.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class InvisibilityPotionPickup : MonoBehaviour
 {
+    //public AudioClip lootSFX;
+    int invisPotionCounter = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.Rotate(Vector3.forward, 90 * Time.deltaTime);
+
+        //if (transform.position.y < Random.Range(4.0f, 7.0f))
+        //{
+        //    Destroy(gameObject.GetComponent<Rigidbody>());
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            invisPotionCounter++;
+            //AudioSource.PlayClipAtPoint(lootSFX, transform.position);
+            print(invisPotionCounter);
+
+            Destroy(gameObject, 0.5f);
+        }
     }
 }
