8dbcf09b7aef5b2f3709835bd8c856843bf071ce
Richa Juvekar <richa.juvekar@gmail.com>
2024-05-16 12:50:27 -0400

    Fixed invisibility potion behavior
    - Instructions update when space is pressed to deploy potion
    - Created separate PlayerInvisibility script to handle invisibility effects on player when space is pressed
    - Need to fix counter so that it decreases when a potion is used

--- Assets/UnityTechnologies/Scripts/InvisibilityPotionPickup.cs
+++ Assets/UnityTechnologies/Scripts/InvisibilityPotionPickup.cs
@@ -1,46 +1,19 @@
-using System.Collections;
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 public class InvisibilityPotionPickup : MonoBehaviour
 {
-    //public AudioClip lootSFX;
     public TMP_Text potionCounter;
-    public TMP_Text instructions;
-    public static bool isInvisible;
-    int invisPotionCounter = 0;
+    public static int invisPotionCounter = 0;
 
     void Start()
     {
-        potionCounter.text = "Invisibility Potions: 0";
-        instructions.text = "To enable invisibility, press space!";
-        isInvisible = false;
+        potionCounter.text = "Invisibility Potions: " + invisPotionCounter;
     }
 
     void Update()
     {
-        Debug.Log("Update method is running");
-        if (invisPotionCounter > 0 && !isInvisible)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                print("Space key pressed");
-                StartCoroutine(ActivateInvisibility());
-            }
-        }
-    }
-
-    IEnumerator ActivateInvisibility()
-    {
-        isInvisible = true;
-        print("ur invis");
-        invisPotionCounter--;
         potionCounter.text = "Invisibility Potions: " + invisPotionCounter;
-        instructions.text = "You're invisible! Run!";
-
-        yield return new WaitForSeconds(5f);
-
-        isInvisible = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -50,9 +23,7 @@ public class InvisibilityPotionPickup : MonoBehaviour
             gameObject.SetActive(false);
             invisPotionCounter++;
             potionCounter.text = "Invisibility Potions: " + invisPotionCounter;
-            //AudioSource.PlayClipAtPoint(lootSFX, transform.position);
-            print(invisPotionCounter);
-
+            // play a sound effect here
             Destroy(gameObject, 0.5f);
         }
     }
---
+++ Assets/UnityTechnologies/Scripts/PlayerInvisibility.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInvisibility : MonoBehaviour
+{
+    public TMP_Text instructions;
+    public static bool isInvisible;
+
+    void Start()
+    {
+        instructions.text = "To enable invisibility, press space!";
+        isInvisible = false;
+    }
+
+    void Update()
+    {
+        if (InvisibilityPotionPickup.invisPotionCounter > 0 && !isInvisible)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(ActivateInvisibility());
+            }
+        }
+    }
+
+    IEnumerator ActivateInvisibility()
+    {
+        isInvisible = true;
+        InvisibilityPotionPickup.invisPotionCounter--;
+        instructions.text = "You're invisible! Run!";
+
+        yield return new WaitForSeconds(5f);
+
+        isInvisible = false;
+        instructions.text = "To enable invisibility, press space!";
+    }
+}
